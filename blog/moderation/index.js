cffb68863d6c50cb48f59f73d253fd6ec3fa05eb
Fock Space <a86gj3sp4@gmail.com>
2023-01-14 20:05:51 +0800

    work with data detail

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -11,14 +11,9 @@ app.post("/events", async (req, res) => {
   if (type === "CommentCreated") {
     const status = data.content.includes("orange") ? "rejected" : "approved";
 
-    await axios.post('http://localhost:4005/events', {
-      type: 'CommentModerated',
-      data: {
-        id: data.id,
-        postId: data.postId,
-        status,
-        content: data.content
-      }
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: { ...data, status },
     });
   }
 
